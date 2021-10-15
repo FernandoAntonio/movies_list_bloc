2ab07d7f08abf610eeeaac3056ac2e11b80b94fc
Fernando Antonio <30207066+fernandoantonio@users.noreply.github.com>
2021-10-15 19:30:22 -0300

    Updated for Store
    Signed-off-by: Fernando Antonio <30207066+fernandoantonio@users.noreply.github.com>

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,8 +2,7 @@
 // Generated file. Do not edit.
 //
 
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-// ignore_for_file: directives_ordering
+import 'package:flutter_web_plugins/flutter_web_plugins.dart'; // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:image_picker_for_web/image_picker_for_web.dart';
